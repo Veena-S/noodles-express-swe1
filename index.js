25c070bdd7ee477a72cc0592844b3c3c782670ec
Veena <veenasivan@gmail.com>
2020-11-12 20:32:27 +0800

    Completed version

--- index.js
+++ index.js
@@ -47,7 +47,79 @@ const getRecipeByYield = (request, response) => {
   });
 };
 
+const getRecipeByLabelUsingHyphen = (request, response) => {
+  read('data.json', (jsonObjectData) => {
+    const requestedLabel = `${request.params.label1} ${request.params.label2} ${request.params.label3}`;
+    console.log('requestedLabel: ', requestedLabel);
+    // Filter out the recipes that are not having a yield field.
+    const recipesWithLabel = jsonObjectData.recipes.filter((element) => ('label' in element));
+    if (recipesWithLabel === undefined || recipesWithLabel === null
+      || recipesWithLabel.length === 0)
+    {
+      response.set(404).send('Sorry, we cannot find that!.');
+      return;
+    }
+    console.log('recipesWithLabel count: ', recipesWithLabel.length);
+
+    // Create the list of recipes that matches the specified label
+    const responseRecipeList = [];
+    recipesWithLabel.forEach((element) => {
+      if (element.label.toLowerCase() === requestedLabel)
+      {
+        console.log('Found matching element: ', element);
+        responseRecipeList.push(element);
+      }
+    });
+    console.log('responseRecipeList count: ', responseRecipeList.length);
+
+    console.log();
+    response.send(responseRecipeList);
+  });
+};
+
+const getRecipeByLabelUsingRegEx = (request, response) => {
+  read('data.json', (jsonObjectData) => {
+    let requestedLabel = request.params.label;
+    // Replace all the - in requested label
+    requestedLabel = requestedLabel.replace(/-/g, ' ');
+    console.log('requestedLabel: ', requestedLabel);
+    // Filter out the recipes that are not having a yield field.
+    const recipesWithLabel = jsonObjectData.recipes.filter((element) => ('label' in element));
+    if (recipesWithLabel === undefined || recipesWithLabel === null
+      || recipesWithLabel.length === 0)
+    {
+      response.set(404).send('Sorry, we cannot find that!.');
+      return;
+    }
+    console.log('recipesWithLabel count: ', recipesWithLabel.length);
+
+    // Create the list of recipes that matches the specified label
+    const responseRecipeList = [];
+    recipesWithLabel.forEach((element) => {
+      if (element.label.toLowerCase() === requestedLabel)
+      {
+        console.log('Found matching element: ', element);
+        responseRecipeList.push(element);
+      }
+    });
+    console.log('responseRecipeList count: ', responseRecipeList.length);
+
+    console.log();
+    response.send(responseRecipeList);
+  });
+};
+
 app.get('/recipe/:index', getRecipeByIndex);
 app.get('/yield/:index', getRecipeByYield);
+
+// Reference: https://expressjs.com/th/guide/routing.html
+// Since the hyphen (-) and the dot (.) are interpreted literally,
+// they can be used along with route parameters
+// for each words, add separate routes
+app.get('/recipe-label/:label1-:label2-:label3', getRecipeByLabelUsingHyphen);
+
+// // The above can be acheived by using regular expressions also
+// app.get('/recipe-label/:label(/[a-z]{0,}-{0,}/gi)', getRecipeByLabelUsingRegEx);
+// app.get('/recipe-label/:label', getRecipeByLabelUsingRegEx);
 
 app.listen(PORT);
