574e204eeaca31e51f6b1508f6583ea751baf3bf
Veena <veenasivan@gmail.com>
2020-11-12 18:46:09 +0800

    Done search by yield. Completed Comfortable

--- index.js
+++ index.js
@@ -19,6 +19,35 @@ const getRecipeByIndex = (request, response) => {
   });
 };
 
+const getRecipeByYield = (request, response) => {
+  read('data.json', (jsonObjectData) => {
+    const requestedYield = Number(request.params.index);
+    console.log('requestedYield: ', requestedYield);
+    // Filter out the recipes that are not having a yield field.
+    const recipesWithYield = jsonObjectData.recipes.filter((element) => ('yield' in element));
+    if (recipesWithYield === undefined || recipesWithYield === null
+      || recipesWithYield.length === 0)
+    {
+      response.set(404).send('Sorry, we cannot find that!.');
+      return;
+    }
+    console.log('recipesWithYield count: ', recipesWithYield.length);
+    // Create the list of recipes that matches the specified yield value
+    const responseRecipeList = [];
+    recipesWithYield.forEach((element) => {
+      console.log(Number(element.yield));
+      if (Number(element.yield) === requestedYield)
+      {
+        console.log('Found matching element: ', element);
+        responseRecipeList.push(element);
+      }
+    });
+    console.log('responseRecipeList count: ', responseRecipeList.length);
+    response.send(responseRecipeList);
+  });
+};
+
 app.get('/recipe/:index', getRecipeByIndex);
+app.get('/yield/:index', getRecipeByYield);
 
 app.listen(PORT);
