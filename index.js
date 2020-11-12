f9e56c27c82ddd8e101316602abbb5f7e16a87eb
Veena <veenasivan@gmail.com>
2020-11-12 18:15:49 +0800

    Completed GetByIndex

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const PORT = 3004;
+const app = express();
+
+const getRecipeByIndex = (request, response) => {
+  read('data.json', (jsonObjectData) => {
+    const countRecipes = jsonObjectData.recipes.length;
+    console.log(`Count: ${countRecipes}`);
+    if (request.index >= countRecipes)
+    {
+      response.send(`Invalid index. There are only ${countRecipes} recipes available.`);
+      return;
+    }
+    console.log('jsonObjectData.recipes[request.index]: ', jsonObjectData.recipes[request.params.index]);
+    response.send(jsonObjectData.recipes[request.params.index]);
+  });
+};
+
+app.get('/recipe/:index', getRecipeByIndex);
+
+app.listen(PORT);
